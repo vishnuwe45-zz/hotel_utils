f896325c203ecd4d7465671e86090212b3fc02ef
vishnuwe45 <vishnu.prasad@we45.com>
2017-01-11 11:39:53 +0530

    Added wallet info

--- server.js
+++ server.js
@@ -16,18 +16,23 @@ app.listen(3001, function () {
   console.log('Example app listening on port 3001!')
   
 })
-// $.ajax({
-//     url : "/api/wallet/gfgdgdf/", 
-//     type : "PUT", 
-//     beforeSend: function(request) {
-//         request.setRequestHeader("Content-Type", "application/json");
-//         request.setRequestHeader("Authorization", "Bearer bgfdsdfsdf");
-//     },
-//     data : JSON.stringify({"wallet_points":100}),     
-//     success : function(json) {                    
-//         console.log(json); 
-//     },    
-//     error : function(xhr,errmsg,err) {                
-//         console.log(xhr.status + ": " + xhr.responseText); 
-//     }
-// });
+
+$.ajax({
+    url : "/api/wallet/<wallet_id>/", 
+    type : "PUT", 
+    beforeSend: function(request) {
+        request.setRequestHeader("Content-Type", "application/json");
+        request.setRequestHeader("Authorization", "Bearer <oauth_token>");
+    },
+    data : JSON.stringify(
+        {
+            "wallet_points":100,
+            "global_wallet_id":"<global_wallet_id>"
+        }),     
+    success : function(json) {                    
+        console.log(json); 
+    },    
+    error : function(xhr,errmsg,err) {                
+        console.log(xhr.status + ": " + xhr.responseText); 
+    }
+});
